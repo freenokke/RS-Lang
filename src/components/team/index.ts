e4c8c68c72d8d498efc2af55dd08c6dae43855a5
Artem <tema.shevchuk@gmail.com>
2022-08-25 21:28:08 +0300

    feat: add about team component

--- src/components/team/index.ts
+++ src/components/team/index.ts
@@ -1,7 +1,20 @@
 import Page from '../helpers/page';
+import './style.scss';
+import Template from './index.html';
+import MaximPhoto from './assets/maksim_photo_multi.webp';
+import GithubIcon from './assets/github_icon.svg';
+import ArtemPhoto from './assets/artem_photo_multi.webp';
+import JanPhoto from './assets/jan_photo_multi.webp';
+
+const model = {
+  maximPhoto: MaximPhoto,
+  githubIcon: GithubIcon,
+  artemPhoto: ArtemPhoto,
+  janPhoto: JanPhoto,
+};
 
 export default class Team extends Page {
   constructor(parentNode: HTMLElement | null) {
-    super('div', [], parentNode, 'About team', {});
+    super('div', [], parentNode, Template, model);
   }
 }
