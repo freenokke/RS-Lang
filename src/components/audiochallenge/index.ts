1c58ed2a651bf1ce9fd6afa15bc7f24e47a46339
janChorny <janchorny@tut.by>
2022-08-26 17:32:46 +0300

    feat: add general audiochallenge game component

--- src/components/audiochallenge/index.ts
+++ src/components/audiochallenge/index.ts
@@ -1,7 +1,9 @@
 import Page from '../helpers/page';
+import Template from './index.html';
+import './style.scss';
 
 export default class Audiochallenge extends Page {
   constructor(parentNode: HTMLElement | null) {
-    super('div', [], parentNode, 'Audiochallenge', {});
+    super('div', [], parentNode, Template, {});
   }
 }
