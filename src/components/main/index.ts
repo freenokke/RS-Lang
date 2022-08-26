e90bb86db8de68e63577145952c14dfc94fd82ea
freenokke <kosmatkov94@gmail.com>
2022-08-26 17:51:34 +0300

    feat: add event for registration button

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -6,6 +6,7 @@ import DoneIcon from './img/done.svg';
 import PeopleIcon from './img/people_hi.svg';
 import Template from './index.html';
 import './style.scss';
+import Pages from '../../enum/routing';
 
 const model = {
   libraryIcon: LibraryIcon,
@@ -16,7 +17,17 @@ const model = {
 };
 
 export default class Main extends Page {
+  private registerButton;
+
   constructor(parentNode: HTMLElement | null) {
     super('main', [], parentNode, Template, model);
+    this.registerButton = this.node.querySelector('.registration__button');
+    this.initEventListeners();
+  }
+
+  private initEventListeners() {
+    this.registerButton.addEventListener('click', () => {
+      window.location.hash = Pages.registration;
+    });
   }
 }
