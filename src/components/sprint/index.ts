fb61dfbabc32cf9eba2e671407bc98f60498e963
janChorny <janchorny@tut.by>
2022-08-29 01:24:01 +0300

    feat: add timer to sprint game

--- src/components/sprint/index.ts
+++ src/components/sprint/index.ts
@@ -4,6 +4,10 @@ import './style.scss';
 import { IWord } from '../../types/words';
 
 export default class Sprint extends Page {
+  static wrongButton: HTMLElement;
+  static rightButton: HTMLElement;
+  static countdownNumberEl: HTMLElement;
+
   constructor(
     wordsList: IWord[],
     comebackHash: string,
@@ -16,5 +20,23 @@ export default class Sprint extends Page {
       Template,
       {}
     );
+  }
+
+  static sprintListener() {
+    this.wrongButton = document.querySelector('.false');
+    this.wrongButton.addEventListener('click', () => console.log('Wrong'));
+    this.rightButton = document.querySelector('.true');
+    this.rightButton.addEventListener('click', () => console.log('Right'));
+  }
+
+  static startCountDown() {
+    this.countdownNumberEl = document.getElementById('countdown-number');
+    let countdown = 60;
+    this.countdownNumberEl.textContent = `${countdown}`;
+    setInterval(() => {
+      /* eslint-disable-next-line no-plusplus */
+      countdown = --countdown <= 0 ? 60 : countdown;
+      this.countdownNumberEl.textContent = `${countdown}`;
+    }, 1000);
   }
 }
